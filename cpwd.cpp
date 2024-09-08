08389cbb5e7d63ac3bd46a8518fed8fe818e4086
A92LEKSANDR <k92aco@gmail.com>
2024-09-08 15:07:35 +0300

    add struct

--- cpwd.cpp
+++ cpwd.cpp
@@ -10,73 +10,24 @@ std::string getCurrentPath(){
     char buffer[PATH_MAX];
     
     if(getcwd(buffer,sizeof(buffer)) != nullptr){
-        return std::string(buffer,strlen(buffer));      //safe construction dont use '/0'
+        return std::string("cd ") + std::string(buffer,strlen(buffer));      //safe construction dont use '/0'
     }else{
         perror("getcwd");
         return "";
     }
 }
 
-void copyToClipboard(Display* display, const std::string& path){
-    Window window = XCreateSimpleWindow(display, DefaultRootWindow(display),0,0,1,1,0,0,0);
-
-    Atom cpboard = XInternAtom(display, "CLIPBOARD", False);
-    Atom targetsAtom  = XInternAtom(display, "TARGETS", False);
-    Atom utf8Str = XInternAtom(display, "UFT8_STRING", False);
-    Atom textAtom = XInternAtom(display, "TEXT", False);
-
-    XSetSelectionOwner(display,cpboard,window,CurrentTime);
-
-    if(XGetSelectionOwner(display,cpboard) != window){
-        std::cerr << "Нет владельца буфера\n";
-        return;
-    }
-
-    XEvent event;
-
-    while(true){
-    
-        XNextEvent(display, &event);
-        if(event.type == SelectionRequest){
-            XSelectionRequestEvent* req = &event.xselectionrequest;
-
-            XSelectionEvent selEvent = {0};
-            selEvent.type = SelectionNotify;
-            selEvent.display = req->display;
-            selEvent.requestor = req->requestor;
-            selEvent.selection = req->selection;
-            selEvent.target = req->target;
-            selEvent.property = None;
-            selEvent.time = req->time;
-
-            if (req->target == targetsAtom) {
-                Atom availableTargets[] = {utf8Str, textAtom};
-                XChangeProperty(display, req->requestor, req->property, XA_ATOM, 32, PropModeReplace, (unsigned char*)availableTargets, 2);
-                selEvent.property = req->property;
-            } else if (req->target == utf8Str || req->target == textAtom) {
-                XChangeProperty(display, req->requestor, req->property, req->target, 8, PropModeReplace, (unsigned char*)path.c_str(), path.length());
-                selEvent.property = req->property;
-            }
-
-            XSendEvent(display, req->requestor, False, 0, (XEvent*)&selEvent);
-            XFlush(display);
-
-       }
-    }
+void copyToClipBoard(const std::string& text){
+    std::string system_command = "echo -n \"" + text + "\" | wl-copy";
+    system(system_command.c_str());  // Выполнение команды для копирования в буфер обмена
 }
 
 int main() {
-    Display* display = XOpenDisplay(nullptr);
-    if (!display) {
-        std::cerr << "Не удалось подключиться к дисплею X11\n";
-        return 1;
-    }
-
+    
     std::string path = getCurrentPath();
-    if (!path.empty()) {
-        copyToClipboard(display, path);
+    
+    if(!path.empty()){
+        copyToClipBoard(path);
     }
-
-    XCloseDisplay(display);
     return 0;
 }
