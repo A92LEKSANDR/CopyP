06deab9849875f32cb9d01cdad7e0144539b1af4
A92LEKSANDR <k92aco@gmail.com>
2024-09-08 15:37:37 +0300

    update binary

--- install.sh
+++ install.sh
@@ -6,19 +6,27 @@ BINARY_NAME="cpwd"
 # Путь к домашней директории пользователя
 BIN_DIR="$HOME/bin"
 
+# Проверка, находится ли бинарный файл рядом со скриптом
+SCRIPT_DIR="$(dirname "$0")"
+BINARY_PATH="$SCRIPT_DIR/$BINARY_NAME"
+
+if [ ! -f "$BINARY_PATH" ]; then
+    echo "Ошибка: бинарник $BINARY_NAME не найден в папке $SCRIPT_DIR."
+    exit 1
+fi
+
 # Создание директории ~/bin, если она не существует
 if [ ! -d "$BIN_DIR" ]; then
     echo "Создаю директорию $BIN_DIR..."
     mkdir -p "$BIN_DIR"
 fi
 
-# Проверка, есть ли уже бинарник в ~/bin
+# Перемещение бинарного файла в ~/bin
 if [ -f "$BIN_DIR/$BINARY_NAME" ]; then
     echo "Бинарник $BINARY_NAME уже существует в $BIN_DIR"
 else
-    # Перемещение бинарного файла в ~/bin
     echo "Перемещаю $BINARY_NAME в $BIN_DIR..."
-    mv "$BINARY_NAME" "$BIN_DIR/"
+    mv "$BINARY_PATH" "$BIN_DIR/"
 fi
 
 # Добавление ~/bin в PATH, если его там нет
